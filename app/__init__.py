5592f27f842db55fa9ef58e1b2921865971ea6dc
Nicolas Giussani Viera <nicolas.vieira@sou.unaerp.edu.br>
2025-04-21 01:29:13 -0300

    Solução para site

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from .routes import main_bp
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='templates')
     app.register_blueprint(main_bp)
     return app
